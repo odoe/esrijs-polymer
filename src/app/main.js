39b8a5d4c86c79bd7cbeca7707782ad4a6fec388
rene rubalcava <rrubalcava@esri.com>
2016-02-25 10:38:43 -0800

    add a pretty layer

--- src/app/main.js
+++ src/app/main.js
@@ -1,16 +1,20 @@
 import Map from 'esri/Map';
+import FeatureLayer from 'esri/layers/FeatureLayer';
 import MapView from 'esri/views/MapView';
 import PolymerController from 'app/views/PolymerView/Controller';
 
 let map = new Map({
-  basemap: 'dark-gray'
+  basemap: 'dark-gray',
+  layers: [
+    new FeatureLayer({ url: 'http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/GeoFRED_Per_Capita_Personal_Income_by_County_Dollars/FeatureServer/0' })
+  ]
 });
 
 let view = new MapView({
   map: map,
   container: 'viewDiv',
-  center: [-118.078, 34.117],
-  zoom: 12
+  center: [-96.786, 37.051],
+  zoom: 5
 });
 
 view.then(() => {
